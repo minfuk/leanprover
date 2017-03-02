4148eb558e8eec221c12d8a65fab4341f44e4148
minfuk <m-fukushima@mopera.net>
2017-03-02 16:44:43 +0900

    名前を変更

--- lean3/math/category/natural_transformation.lean
+++ lean3/math/category/natural_transformation.lean
@@ -9,9 +9,9 @@ import .functor
 open category functor
 
 structure natural_transformation {C D : Category} (F G : C ⇒ D) :=
-(η : Π(a : C), @hom _ D^.struct (F a) (G a))
+(eta : Π(a : C), @hom _ D^.struct (F a) (G a))
 (commute : Π {a b : C} (f : @hom _ C^.struct a b),
-  @comp _ D^.struct _ _ _ (G^.morphism f) (η a) = @comp _ D^.struct _ _ _ (η b) (F^.morphism f))
+  @comp _ D^.struct _ _ _ (G^.morphism f) (eta a) = @comp _ D^.struct _ _ _ (eta b) (F^.morphism f))
 
 infixl `⟹`:25 := natural_transformation -- \==>
 
@@ -21,7 +21,7 @@ namespace natural_transformation
   variables {F G H I : Functor C D}
 
   instance natural_map : has_coe_to_fun (F ⟹ G) :=
-  {F := _, coe := λ m, m^.η}
+  {F := _, coe := λ m, m^.eta}
 
   theorem naturality (η : F ⟹ G) : Π⦃a b : C⦄ (f : @hom _ C^.struct a b),
     @comp _ D^.struct _ _ _ (G^.morphism f) (η a) =
@@ -47,14 +47,14 @@ namespace natural_transformation
       η₃ ∘n (η₂ ∘n η₁) = (η₃ ∘n η₂) ∘n η₁ :=
   let infixr `⟶` := λ(a b : D), @hom _ D^.struct a b in
   let infixr `∘` := λ{a b c : D} (f : b ⟶ c) (g : a ⟶ b), @comp _ D^.struct _ _ _ f g in
-  have H : (η₃ ∘n (η₂ ∘n η₁))^.η = ((η₃ ∘n η₂) ∘n η₁)^.η, from
+  have H : (η₃ ∘n (η₂ ∘n η₁))^.eta = ((η₃ ∘n η₂) ∘n η₁)^.eta, from
     have H1 : ∀(a : C), (η₃ a) ∘ ((η₂ a) ∘ (η₁ a)) = ((η₃ a) ∘ (η₂ a)) ∘ (η₁ a), from
       take a,
       @assoc _ D^.struct _ _ _ _ (η₃ a) (η₂ a) (η₁ a),
-    calc (η₃ ∘n (η₂ ∘n η₁))^.η
+    calc (η₃ ∘n (η₂ ∘n η₁))^.eta
           = λ(a : C), (η₃ a) ∘ ((η₂ a) ∘ (η₁ a)) : rfl
       ... = λ(a : C), ((η₃ a) ∘ (η₂ a)) ∘ (η₁ a) : funext H1
-      ... = ((η₃ ∘n η₂) ∘n η₁)^.η : rfl,
+      ... = ((η₃ ∘n η₂) ∘n η₁)^.eta : rfl,
   begin
     cases η₃ ∘n (η₂ ∘n η₁),
     cases (η₃ ∘n η₂) ∘n η₁,
@@ -82,28 +82,28 @@ namespace natural_transformation
   have Ha : ∀(a : α), f a b₁ = f a b₂, from take a, congr_arg (λb, f a b) H2,
   calc f a₁ b₁ = f a₂ b₁ : Hb b₁ ... = f a₂ b₂ : Ha a₂
 
-  protected theorem id_left (η₁ : F ⟹ G) : natural_transformation.compose natural_transformation.id η₁ = η₁ :=
-  have H : (natural_transformation.compose natural_transformation.id η₁)^.η = η₁^.η, from
-    have H' : ∀(a : C), @comp _ D^.struct _ _ _ (@ID _ D^.struct (G a)) (η₁ a) = (η₁ a), from
+  protected theorem id_left (η : F ⟹ G) : natural_transformation.compose natural_transformation.id η = η :=
+  have H : (natural_transformation.compose natural_transformation.id η)^.eta = η^.eta, from
+    have H' : ∀(a : C), @comp _ D^.struct _ _ _ (@ID _ D^.struct (G a)) (η a) = (η a), from
       take a,
-      @id_left _ D^.struct _ _ (η₁ a),
+      @id_left _ D^.struct _ _ (η a),
     funext H',
   begin
-    cases (natural_transformation.compose natural_transformation.id η₁),
-    cases η₁,
+    cases (natural_transformation.compose natural_transformation.id η),
+    cases η,
     cases H,
     apply rfl
   end
 
-  protected theorem id_right (η₁ : F ⟹ G) : natural_transformation.compose η₁ natural_transformation.id = η₁ :=
-  have H : (natural_transformation.compose η₁ natural_transformation.id)^.η = η₁^.η, from
-    have H' : ∀(a : C), @comp _ D^.struct _ _ _ (η₁ a) (@ID _ D^.struct (F a)) = (η₁ a), from
+  protected theorem id_right (η : F ⟹ G) : natural_transformation.compose η natural_transformation.id = η :=
+  have H : (natural_transformation.compose η natural_transformation.id)^.eta = η^.eta, from
+    have H' : ∀(a : C), @comp _ D^.struct _ _ _ (η a) (@ID _ D^.struct (F a)) = (η a), from
       take a,
-      @id_right _ D^.struct _ _ (η₁ a),
+      @id_right _ D^.struct _ _ (η a),
     funext H',
   begin
-    cases natural_transformation.compose η₁ natural_transformation.id,
-    cases η₁,
+    cases natural_transformation.compose η natural_transformation.id,
+    cases η,
     cases H,
     apply rfl
   end

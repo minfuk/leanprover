417b1249092be9b1d75c3a7d5eead4f278ad34c7
minfuk <m-fukushima@mopera.net>
2017-03-05 17:28:35 +0900

    diag_functor 追加

---
+++ lean3/math/category/diag.lean
@@ -0,0 +1,16 @@
+import .category
+import .functor
+import .construction
+
+namespace category.diag
+
+  open category.ops
+
+  definition diag_functor {C : Category} : C ⇒ (C ×c C) :=
+  Functor.mk
+    (λa, (a, a) )
+    (λa b f, (f, f) )
+    (λa, rfl)
+    (λa b c g f, rfl)
+
+end category.diag
